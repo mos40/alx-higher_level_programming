ab5df7cd69977e5bc40c6ac07d394b1dd71ad239
Moses <mosesmkhonza@outlook.com>
2023-11-30 14:18:24 -0800

    argus

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 from sys import argv
 
 num_args = len(argv) - 1
-print("{} argument{}{}".format(num_args, 's' if num_args != 1 else '', ':' if num_args > 0 else '.'))
+print("{} argument{}{}".format(num_args, 's'
+    if num_args != 1 else '', ':' if num_args > 0 else '.'))
 
 for i in range(1, num_args + 1):
     print("{}: {}".format(i, argv[i]))
