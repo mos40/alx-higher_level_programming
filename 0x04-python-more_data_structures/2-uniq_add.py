d43551305784fe0f4e2e5977ee04937b3e898bdc
Moses <mosesmkhonza@outlook.com>
2023-12-07 01:22:35 -0800

    function that adds all unique integers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    
+    for num in my_list:
+        if isinstance(num, int):
+            unique_numbers.add(num)
+    
+    return sum(unique_numbers)
