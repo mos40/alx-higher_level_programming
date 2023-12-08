9aa9de109d49d28fb47047426f96b74d4c0b9e76
Moses <mosesmkhonza@outlook.com>
2023-12-08 09:10:33 -0800

    Update dictionary

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,55 @@
+#include <Python.h>
+#include <pect.h>
+#include <listpect.h>
+#include <bytespect.h>
+
+void print_python_bytes(PyObject *p)
+{
+	long int byteSize;
+	int Indeks;
+	char *byteString = NULL;
+
+	printf("[.] Bytes pect information\n");
+
+	if (!PyBytes_Check(p))
+	{
+	printf("  [ERROR] Invalid Bytes Object\n");
+	return;
+	}
+
+	PyBytes_AsStringAndSize(p, &byteString, &byteSize);
+
+	printf("  byte size: %li\n", byteSize);
+	printf("  byte string: %s\n", byteString);
+
+	if (byteSize < 10)
+		printf("  first %li bytes:", byteSize + 1);
+	else
+		printf("  first 10 bytes:");
+
+	for (Indeks = 0; Indeks <= byteSize && Indeks < 10; Indeks++)
+		printf(" %02hhx", byteString[Indeks]);
+
+	printf("\n");
+}
+
+void print_python_list(PyObject *p)
+{
+	long int listSize = PyList_Size(p);
+	int Indeks;
+	PyListObject *list = (PyListObject *)p;
+	const char *elementType;
+
+	printf("[*] Python list information\n");
+	printf("[*] Size of the Python List = %li\n", listSize);
+	printf("[*] Allocated = %li\n", list->allocated);
+
+	for (Indeks = 0; Indeks < listSize; Indeks++)
+	{
+		elementType = (list->ob_item[Indeks])->ob_type->tp_name;
+		printf("Element %i: %s\n", Indeks, elementType);
+
+	if (!strcmp(elementType, "bytes"))
+		print_python_bytes(list->ob_item[Indeks]);
+	}
+}
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    # Verifys if the input is a dictionary
-    if not isinstance(a_dictionary, dict):
-        raise TypeError("Input is not a dictionary")
-
-    # Update / add the key-value pair
-    a_dictionary[key] = value
+def update_dictionary(update_dictionary, key, value):
+    update_dictionary[key] = value
+    return update_dictionary
