54d922e93d817fa7c6f5cf893d866ed6ed03fb30
Moses <mosesmkhonza@outlook.com>
2023-12-08 04:42:08 -0800

    bfunction that deletes a key in a dictionary.

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    # Check if the key exists in the dictionary
+    if key in a_dictionary:
+        # Delete the key-value pair if it exists
+        del a_dictionary[key]
+
+    # Return the modified dictionary
+    return a_dictionary
