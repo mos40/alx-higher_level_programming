a2ff9986e4d216a436c80819b973de16789cb9f0
Mos40 <mosesmkhonza@outlook.com>
2023-12-19 16:26:32 -0800

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 
-"""Define classes for a singly-linked list."""
+"""Define a class Square."""
 
 
 class Node:
-    """Represent a node in a singly-linked list."""
+    """Represent a square."""
 
-    def __init__(self, data, next_node=None):
-        """Initialize a new Node.
+    def __init__(self, size, position=None):
+        """Initialize a new square.
 
         Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        self.data = data
-        self.next_node = next_node
+        self._size = size
+        self._position = position
 
     @property
-    def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+    def size(self):
+        """Get/set the current size size of the square."""
+        return (self.__size)
 
-    @data.setter
-    def data(self, value):
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
+            raise TypeError("size must be an integer")
+        self.__size = value
 
     @property
-    def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+    def position(self):
+        """Get/set the position of the Node."""
+        return (self.__position)
 
-    @next_node.setter
-    def next_node(self, value):
+    @position.setter
+    def position(self, value):
         if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+            raise TypeError("position must be a Node object")
+        self.__position = value
 
 
 class SinglyLinkedList:
@@ -57,24 +57,24 @@ class SinglyLinkedList:
         """
         new = Node(value)
         if self.__head is None:
-            new.next_node = None
+            new.position = None
             self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
+        elif self.__head.size > value:
+            new.position = self.__head
             self.__head = new
         else:
             tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+            while (tmp.position is not None and
+                    tmp.position.size < value):
+                tmp = tmp.position
+            new.position = tmp.position
+            tmp.position = new
 
     def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
+        """Define the print() representation of a Square."""
         values = []
         tmp = self.__head
         while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
+            values.append(str(tmp.size))
+            tmp = tmp.position
         return ('\n'.join(values))
