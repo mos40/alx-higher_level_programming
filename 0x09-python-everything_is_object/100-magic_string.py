4bc0407691dcf1e65832436da3c722e5e0ff1383
Mos40 <mosesmkhonza@outlook.com>
2024-01-05 13:36:35 -0800

    Def

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def generate_magic_string():
-    generate_magic_string.count = getattr(generate_magic_string, 'count', 0) + 1
-    return ("BestSchool, " * (generate_magic_string.count - 1) + "BestSchool")
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
