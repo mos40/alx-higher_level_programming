5b7329c1b2cffea868cbde471b5e0dbcec43d475
Mos40 <mosesmkhonza@outlook.com>
2024-01-06 07:15:53 -0800

    class Rectangle that defines a rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,1 +1,97 @@
-u
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        type(self).number_of_instances += 1
+        self._width = 0
+        self._height = 0
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the Rectangle."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._validate_dimension(value, "width")
+        self._width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the Rectangle."""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._validate_dimension(value, "height")
+        self._height = value
+
+    def _validate_dimension(self, value, dimension):
+        """Validate if the given value is a valid dimension."""
+        if not isinstance(value, int):
+            raise TypeError(f"{dimension} must be an integer")
+        if value < 0:
+            raise ValueError(f"{dimension} must be >= 0")
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self._width * self._height
+
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._width + self._height)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the Rectangle with the greater area.
+
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+        """
+        if not isinstance(rect_1, Rectangle)
+        or not isinstance(rect_2, Rectangle):
+            raise TypeError("Both rect_1 and rect_2 must be instances of Rect")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
+        if self._width == 0 or self._height == 0:
+            return ""
+        return (str(self.print_symbol) * self._width + "\n") * self._height
+
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        return f"Rectangle({self._width}, {self._height})"
+
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
