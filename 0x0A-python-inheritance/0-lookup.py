5533ff23ff1876d9c3b9675ed0f898874a9edbce
Mos40 <mosesmkhonza@outlook.com>
2024-01-10 12:53:57 -0800

    function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-
-class ExampleClass:
-    def __init__(self, attribute1, attribute2):
-        self.attribute1 = attribute1
-        self.attribute2 = attribute2
-
-    def display_attributes(self):
-        print(f"Attribute 1: {self.attribute1}")
-        print(f"Attribute 2: {self.attribute2}")
+"""Defines a function for object attribute lookup."""
 
 
-if __name__ == "__main__":
-    example_instance = ExampleClass(42, "Hello")
-    example_instance.display_attributes()
+def lookup(obj):
+    """Returns a list of available attributes for an object."""
+    return dir(obj)
