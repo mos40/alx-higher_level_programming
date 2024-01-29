fb87d49e0333928579471d6ad10ea919eb5f281a
Mos40 <mosesmkhonza@outlook.com>
2024-01-29 08:28:14 -0800

    M

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8')
+    as file:
         print(file.read(), end="")
 
 
