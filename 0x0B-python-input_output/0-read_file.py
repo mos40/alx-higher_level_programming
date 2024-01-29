793006fe35c5f5255d7d366d68decbe07ff9005d
Mos40 <mosesmkhonza@outlook.com>
2024-01-29 08:30:21 -0800

    M

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='utf-8')
-    as file:
-        print(file.read(), end="")
-
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
