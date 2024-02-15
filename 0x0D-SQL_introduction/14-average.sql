03fbe2474907aeb57bb30e4805a52ab2d66a9d47
Moses <mosesmkhonza@outlook.com>
2024-02-15 09:40:33 -0800

    List valvues

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,7 +1,4 @@
 -- 14-average.sql
 -- Computes the score average of all records in the table second_table in the specified database
-
-USE `hbtn_0c_0`;  -- Replace 'hbtn_0c_0' with the actual database name
-
-SELECT AVG(score) AS average
-FROM second_table;
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,9 +1,7 @@
 -- 15-groups.sql
 -- Lists the number of records with the same score in the table second_table in the specified database
 
-USE `hbtn_0c_0`;  -- Replace 'hbtn_0c_0' with the actual database name
-
-SELECT score, COUNT(*) as number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC, score DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
