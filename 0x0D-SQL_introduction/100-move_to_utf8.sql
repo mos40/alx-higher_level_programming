ff2bc736454f18ee97e3908d335201e47b06faa7
Moses <mosesmkhonza@outlook.com>
2024-02-15 09:43:19 -0800

    List valvues

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
