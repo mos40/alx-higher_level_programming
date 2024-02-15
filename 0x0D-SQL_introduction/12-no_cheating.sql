3a5fcb37cba7b5a798e107b834aa5d1da9f12445
Moses <mosesmkhonza@outlook.com>
2024-02-15 09:47:50 -0800

    best score

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,8 +1,5 @@
 -- 12-no_cheating.sql
 -- Updates the score of Bob to 10 in the table second_table in the specified database
-
-USE `hbtn_0c_0`;  -- Replace 'hbtn_0c_0' with the actual database name
-
-UPDATE second_table
-SET score = 10
-WHERE name = 'Bob';
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
