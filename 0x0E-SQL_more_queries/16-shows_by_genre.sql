ccef687b1a05638f9a02b35cb814fcfae2444e69
Moses <mosesmkhonza@outlook.com>
2024-02-15 10:41:07 -0800

    SHow by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,5 @@
 -- Task: 16. List shows and genres
 -- Select titles and genres using a LEFT JOIN
-SELECT tv_shows.title, tv_genres.name
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
