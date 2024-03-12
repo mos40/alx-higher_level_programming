5d01106d1b60abf19795ab471efefb821158f22b
Moses <mosesmkhonza@outlook.com>
2024-03-12 13:01:37 -0700

    Rect

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
+// Create an empty object if w or h is not a positive integer or is 0
+// Initialize the instance attributes with the provided values
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {{
-    // Create an empty object if w or h is not a positive integer or is 0
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    // Initialize the instance attributes with the provided values
-    this.width = w;
-    this.height = h;
   }
 }
 
