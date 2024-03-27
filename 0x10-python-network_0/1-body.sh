8a7d5f7f56018e7b98da5d22d0e017be5791c01b
Moses <mosesmkhonza@outlook.com>
2024-03-27 13:40:32 -0700

    delete methods and the body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,15 +2,5 @@
 
 # Sends a request to the given URL and displays the size of the body of the response
 # Usage: ./0-body_size.sh <URL>
-
-# Sending a GET request to the URL and writing the response body to a temporary file
-curl -s "$1" -o temp_body
-
-# Getting the size of the body in bytes
-body_size=$(wc -c < temp_body)
-
-# Displaying the size of the body
-echo "$body_size"
-
-# Removing the temporary file
-rm temp_body
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -2,6 +2,5 @@
 
 # Takes in a URL and displays all HTTP methods the server will accept
 # Usage: ./3-methods.sh <URL>
-
 # Sending an OPTIONS request to the URL and displaying the Allow header
-curl -sI -X OPTIONS "$1" | grep -i Allow | cut -d' ' -f2-
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
